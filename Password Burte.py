4a3335c3ad4d443f5fcd44ad44a8d2ff70fd59d5
Czachord <janek.zimny@gmail.com>
2022-05-31 12:28:37 +0200

    Update Password Burte.py

--- Password Burte.py
+++ Password Burte.py
@@ -1,21 +1,39 @@
 import string
+import datetime
 
-def passInput():
-    password = input("Enter your password (max 5 characters):\n")
+def passInput(letters):
+    password = input("Enter your password:\n")
+    wrongLetters = []
+    for i in range(len(password)):
+        if password[i] not in letters:
+            wrongLetters.append(password[i])
+    if len(wrongLetters) > 0: exit(f'The character(s) {str(wrongLetters)} is not allowed,\nThe only characters allowed are "{string.ascii_letters}"')
+    print("\n")
+    return password
 
-def letters():
-    alphabet = list(string.ascii_letters)
-    return alphabet
+def clearList(list, znak):
+    for e in range(len(list)):
+        list[e] = znak
 
-def combinations(numbers1, numbers2):
-    if numbers2 == len(letters()):
-        numbers1 += 1
-        numbers2 = 0
-
-
+def passBreaking(letters, password):
+    checkPass = [letters[0], ]
+    while checkPass != password:
+        for e in range(len(checkPass)):
+            for i in range(len(letters)):
+                checkPass[e] = letters[i]
+                print(checkPass)
+                if checkPass == list(password):
+                    return checkPass
+                checkPass[e] = letters[0]
+        
+        checkPass.append(letters[0])
+        clearList(checkPass, letters[0])
+        if len(checkPass) > 3: exit()
 
 def main():
-    print(letters())
+    alphabet = string.ascii_letters
+    password = passInput(alphabet)
+    print(str(passBreaking(alphabet, password)))
 
 if __name__ == "__main__":
     main()
